90c9c9a116864f9ef16e291001a62d3cc96ff578
AbrahamSantosSosa <23041214@itdurango.edu.mx>
2024-06-12 17:16:08 -0600

    EXAMEN

---
+++ EXAMEN/src/competencia/Baseball.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package competencia;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Baseball extends Conjunto{
+
+    public Baseball(String asistencias, String faltas, String marcador, String disciplina, String rama, String categoria, String fecha, String hora, String ganador, String segundoLugar, String tercerLugar, String etapa, String sede) {
+        super(asistencias, faltas, marcador, disciplina, rama, categoria, fecha, hora, ganador, segundoLugar, tercerLugar, etapa, sede);
+    }
+    
+}
---
+++ EXAMEN/src/competencia/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package competencia;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main {
+
+    private static String Juan;
+    public static void main(String[] args) throws Exception {
+        Atletismo a1 = new Atletismo();
+        a1.prueba = "100 m";
+        a1.tiempoGanadorr = "10.0";
+        a1.segundoLugar = "Juan";
+        a1.tercerLugar = "Jose";
+        a1.recordPanamericano = "15 seg";
+        
+        System.out.println("Atletismo");
+        System.out.println("La prueba es  "+ a1.getPrueba());
+        System.out.println("El tiempo del ganador de las prueba es 100 " + a1.getTiempoGanador());
+        System.out.println("El segundo lugar de la prueba es " + a1.getSegundoLugar());
+        System.out.println("El tiempo del segundo lugar es " +a1.getTiempo2doLugar());
+        System.out.println("El ganador del tercer lugar es " + a1.getTercerLugar());
+        System.out.println("El nuevo record panamericano  es de " + a1.recordPanamericano());
+    }
+ 
+}
